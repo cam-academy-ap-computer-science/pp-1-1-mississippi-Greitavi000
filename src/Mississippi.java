5cbed83ad2c0aa01263476a331d7ef53a2a56010
greiterruslan <greiterruslan@192.168.7.31>
2019-09-16 15:15:15 -0700

    MississippiIsDone.java

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -23,36 +23,41 @@ public class Mississippi {
 			System.out.println("M   M   M");
 			System.out.println("M       M");
 			System.out.println("M       M");
-			System.out.println("M       M");	
+			System.out.println("M       M");
+			System.out.println("");	
+			
 		}
 	
 		public static void LetterI() {
-			System.out.println("IIIII");
-			System.out.println("  I");
-			System.out.println("  I");
-			System.out.println("  I");
-			System.out.println("  I");
-			System.out.println("  I");
-			System.out.println("IIIII");
+			System.out.println("  IIIII");
+			System.out.println("    I");
+			System.out.println("    I");
+			System.out.println("    I");
+			System.out.println("    I");
+			System.out.println("    I");
+			System.out.println("  IIIII");
+			System.out.println("");
 		}
 		public static void LetterS() {
-			System.out.println("   SSSSSSS");
-			System.out.println("  S");
-			System.out.println("   S");
+			System.out.println(" SSSSSSS");
+			System.out.println("S");
+			System.out.println(" S");
+			System.out.println("    S");
 			System.out.println("      S");
-			System.out.println("        S");
-			System.out.println("         S");
-			System.out.println("  SSSSSSS ");
+			System.out.println("       S");
+			System.out.println("SSSSSSS ");
+			System.out.println("");
 		
 		}
 		public static void LetterP() {
-			System.out.println("PPPP");
-			System.out.println("P   P");
-			System.out.println("P    P");
-			System.out.println("P   P");
-			System.out.println("PPPP");
-			System.out.println("P");
-			System.out.println("P");
+			System.out.println("  PPPP");
+			System.out.println("  P   P");
+			System.out.println("  P    P");
+			System.out.println("  P   P");
+			System.out.println("  PPPP");
+			System.out.println("  P");
+			System.out.println("  P");
+			System.out.println("");
 	}
 
 }
