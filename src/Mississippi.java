939eee299d8be24bb3b1e2148db83f1e5c2d34ad
greitavi000 <greitavi000@CAM-LAB-16.bgsd.k12.wa.us>
2019-09-16 07:54:44 -0700

    ClassProject.java

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -3,7 +3,56 @@ public class Mississippi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		LetterM();
+		LetterI();
+		LetterS();
+		LetterS();
+		LetterI();
+		LetterS();
+		LetterS();
+		LetterI();
+		LetterP();
+		LetterP();
+		LetterI();
+	}
+ 
+		public static void LetterM() {
+			System.out.println("M       M ");
+			System.out.println("M M   M M");
+			System.out.println("M  M M  M");
+			System.out.println("M   M   M");
+			System.out.println("M       M");
+			System.out.println("M       M");
+			System.out.println("M       M");	
+		}
+	
+		public static void LetterI() {
+			System.out.println("IIIII");
+			System.out.println("  I");
+			System.out.println("  I");
+			System.out.println("  I");
+			System.out.println("  I");
+			System.out.println("  I");
+			System.out.println("IIIII");
+		}
+		public static void LetterS() {
+			System.out.println("   SSSSSSS");
+			System.out.println("  S");
+			System.out.println("   S");
+			System.out.println("      S");
+			System.out.println("        S");
+			System.out.println("         S");
+			System.out.println("  SSSSSSS ");
+		
+		}
+		public static void LetterP() {
+			System.out.println("PPPP");
+			System.out.println("P   P");
+			System.out.println("P    P");
+			System.out.println("P   P");
+			System.out.println("PPPP");
+			System.out.println("P");
+			System.out.println("P");
 	}
 
 }
